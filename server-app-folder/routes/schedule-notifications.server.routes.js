cf09168c373d3287f8631d223a1ae1620c9c0fda
Oleksander <oleksander_derechei@epam.com>
2016-12-13 09:24:54 +0200

    Commit rebase

--- server-app-folder/routes/schedule-notifications.server.routes.js
+++
@@ -1,5 +0,0 @@
-module.exports = function(app) {
-	var notifications = require('../controllers/schedule-notifications.server.controller');
-
-	app.route('/notifications').get(notifications.list);
-};
