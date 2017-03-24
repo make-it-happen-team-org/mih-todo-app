4c61a3e3bd238d02d49dfcf91d3f2645179c8bf9
Artem Anashchenkov <artem_anashchenkov@epam.com>
2017-03-24 12:27:28 +0200

    EPMKHRDMH-371 update server.js

--- server-app-folder/server.js
+++ server-app-folder/server.js
@@ -6,32 +6,25 @@ const mongoose = require('mongoose');
 const chalk = require('chalk');
 const mongoHandler = (err) => {
   if (err) {
-    console.error(chalk.red('Could not connect to MongoDB!'));
-    console.log(chalk.red(err));
-  }
-};
-const db = mongoose.connect(config.db.uri, config.db.options, mongoHandler);
-const app = require('../config/express')(db);
-
-mongoose.connection.on('error', function(err) {
     console.error(chalk.red(`MongoDB connection error: ${ err }`));
     process.exit(-1);
   }
-);
 
-// Bootstrap passport config
-require('../config/passport')();
+  const app = require('../config/express')(db);
 
-// Start the server-app-folder by listening on <port>
-app.listen(config.port);
+  // Bootstrap passport config
+  require('../config/passport')();
 
-// Expose server-app-folder
-exports = module.exports = app;
+  // Start the server-app-folder by listening on <port>
+  app.listen(config.port);
 
-// Logging initialization
-console.log('--');
-console.log(chalk.green(`${ config.app.title } application started`));
-console.log(chalk.green(`Environment:\t\t\t${ process.env.NODE_ENV }`));
-console.log(chalk.green(`Port:\t\t\t\t${ config.port }`));
-console.log(chalk.green(`Database:\t\t\t${ config.db.uri }`));
-console.log('--');
+  // Logging initialization
+  console.log('--');
+  console.log(chalk.green(`${ config.app.title } application started`));
+  console.log(chalk.green(`Environment:\t\t\t${ process.env.NODE_ENV }`));
+  console.log(chalk.green(`Port:\t\t\t\t${ config.port }`));
+  console.log(chalk.green(`Database:\t\t\t${ config.db.uri }`));
+  console.log('--');
+
+};
+const db = mongoose.connect(config.db.uri, mongoHandler);
